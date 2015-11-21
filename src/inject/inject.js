d23d07427bf2df64046ea81faaf17988a0b4a0b6
Vadim Sikora <vadim.sikora@divio.ch>
2015-11-21 09:08:53 +0100

    add support for iframes
    injects itself into created iframes and their inner iframes recursively

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,19 +6,9 @@ let SHORTCUT = {
     ctrlKey: false,
     keyCode: 27
 };
-chrome.storage.local.get('SHORTCUT', (value) => {
-    if (value && typeof value.SHORTCUT === 'object') {
-        SHORTCUT = value.SHORTCUT;
-    }
-});
+const INTERVAL = 100;
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
-    if (changes.SHORTCUT) {
-        SHORTCUT = changes.SHORTCUT.newValue;
-    }
-});
-
-chrome.extension.sendMessage({}, (response) => {
+const inject = function inject(document) {
     const readyStateCheckInterval = setInterval(() => {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval);
@@ -34,5 +24,53 @@ chrome.extension.sendMessage({}, (response) => {
                 }
             }, true);
         }
-    }, 10);
+    }, INTERVAL);
+
+    new MutationObserver(mutations => {
+        mutations.some(mutation => {
+            // if iframe is added dynamically to the page
+            if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                const iframes = document.querySelectorAll('iframe');
+
+                Array.from(iframes).forEach(iframe => {
+                    if (!iframe.ready) {
+                        iframe.ready = true;
+                        if (iframe.getAttribute('src')) {
+                            iframe.addEventListener('load', () => {
+                                inject(iframe.contentDocument);
+                            });
+                        } else {
+                            // if iframe has no src but still has content
+                            // or if it doesn't have src at the moment of creation
+                            // should be setInterval probably
+                            setTimeout(() => {
+                                inject(iframe.contentDocument);
+                            }, INTERVAL);
+                        }
+                    }
+                });
+            }
+
+            return false;
+        });
+    }).observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+};
+
+chrome.storage.local.get('SHORTCUT', value => {
+    if (value && typeof value.SHORTCUT === 'object') {
+        SHORTCUT = value.SHORTCUT;
+    }
+});
+
+chrome.storage.onChanged.addListener(changes => {
+    if (changes.SHORTCUT) {
+        SHORTCUT = changes.SHORTCUT.newValue;
+    }
+});
+
+chrome.extension.sendMessage({}, () => {
+    inject(document);
 });
